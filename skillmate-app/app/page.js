672e6f0167a7695970c1f3f503b820554a48d0e6
Brian Onyango <111913340+Bigboss148@users.noreply.github.com>
2025-06-27 11:12:41 -0500

    Home page, DEMO

--- skillmate-app/app/page.js
+++ skillmate-app/app/page.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
-  return <hi>Hello World</hi>
-   
+  return (
+
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <h2 className="text-xl font-semibold mb-4">Featured Skills</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          
+        </div>
+      </main>
+    </div>
+  );
+  
 }
